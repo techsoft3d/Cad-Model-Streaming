123f7b781dc1a9a7a35f01ddaef4b6b9a44006ea
Ricky Johnson <ricky.johnson@techsoft3d.com>
2024-08-22 10:30:43 -0400

    fixed merge error

--- javascripts/common/load_scripts.js
+++ javascripts/common/load_scripts.js
@@ -3,11 +3,7 @@ async function loadDynamicScript() {
   var result = await fetchVersionNumber();
   versionNumer = result['hcVersion']
   // var url = `https://cdn.jsdelivr.net/gh/techsoft3d/hoops-web-viewer@20${versionNumer}/hoops_web_viewer.js`
-<<<<<<< HEAD
-  var url = `https://cdn.jsdelivr.net/gh/techsoft3d/hoops-web-viewer@20${versionNumer}/hoops-web-viewer.iife.js`
-=======
   var url = `https://cdn.jsdelivr.net/gh/techsoft3d/hoops-web-viewer@20${versionNumer}/hoops-web-viewer.mjs`
->>>>>>> 709a14fabd79af2b8c82f5a9009113dea2d3a24b
 
   return new Promise((resolve, reject) => {
       $.getScript(url, async function () {
