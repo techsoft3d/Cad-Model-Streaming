0b99ae2060bbd777c77e9fbf23f1133e020b971e
Ricky Johnson <ricky.johnson@techsoft3d.com>
2024-08-27 13:19:40 -0400

    committing to switch

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -1,9 +1,15 @@
 function loadModels(){
     console.log("loading")
-    hwv.model.loadSubtreeFromScsFile(hwv.model.getRootNode(), "data/smoothed_bracket00.scs").then(() => {
+    hwv.model.loadSubtreeFromScsFile(hwv.model.getRootNode(), "data/00.scs").then(() => {
         console.log('loaded');
       });
-    hwv.model.loadSubtreeFromScsFile(hwv.model.getRootNode(), "data/smoothed_bracket57.scs").then(() => {
+    hwv.model.loadSubtreeFromScsFile(hwv.model.getRootNode(), "data/20.scs").then(() => {
     console.log('loaded');
     });
+    hwv.model.loadSubtreeFromScsFile(hwv.model.getRootNode(), "data/45.scs").then(() => {
+      console.log('loaded');
+      });
+      hwv.model.loadSubtreeFromScsFile(hwv.model.getRootNode(), "data/57.scs").then(() => {
+        console.log('loaded');
+        });
 }
